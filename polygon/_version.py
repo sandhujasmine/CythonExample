e7fa1cd08a09708b4f5e34b24226e3bbec475910
Jasmine Sandhu <jsandhu@continuum.io>
2016-02-27 22:49:27 -0800

    Fix versioneer tag-prefix - remove quotes

--- polygon/_version.py
+++ polygon/_version.py
@@ -36,7 +36,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "'v'"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "polygon-"
     cfg.versionfile_source = "polygon/_version.py"
     cfg.verbose = False
