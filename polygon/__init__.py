743fa2b73617628e43c7658584d04de5e8c73e9b
Jasmine Sandhu <jsandhu@continuum.io>
2016-03-01 10:01:06 -0800

    Moved imports to top of file

--- polygon/__init__.py
+++ polygon/__init__.py
@@ -1,3 +1,6 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 
 def hello_polygon():
@@ -5,6 +8,4 @@ def hello_polygon():
     add this for testing
     """
     print("hello_polygon")
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+
